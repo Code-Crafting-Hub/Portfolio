ec0e0f5498a541bee6ced6e04016ca8a3dd40e0c
Code-Crafting-Hub <yashkaransingh0827@gmail.com>
2026-03-13 17:59:26 +0530

    commit 1303026

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const {app} = require("./app")
 require('dotenv').config()
 
-const port = process.env.PORT || 3002
+// const port = process.env.PORT || 3002
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`server is running on port ${port}`)
+// })
+module.exports = {app}
