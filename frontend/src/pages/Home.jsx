231c08f0e71ff0350d9ef194eee859a8fe2dfd19
Code-Crafting-Hub <yashkaransingh0827@gmail.com>
2026-07-08 13:31:45 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -407,7 +407,7 @@ export default function Home() {
             </div> */}
             <div className="flex justify-center lg:justify-start">
   <a 
-    href="https://drive.google.com/file/d/1zDC2YrhYmJlrTWG6vCckf4-8JowomMH-/view?usp=sharing" 
+    href="https://drive.google.com/file/d/15MVAKYeBWFV_EitmRrVvAA2l_v8ZAZSg/view?usp=drive_link" 
     className="w-fit py-2 px-6 rounded-full bg-[var(--secondary-accent)] hover:bg-[var(--hover-button)]  transition delay-100 duration-300 font-semibold hover:cursor-pointer shadow-md shadow-green-800 text-white"
     target="_blank"
     downlaod="resume"
